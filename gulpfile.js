525dec339c0706b08848937ad8afd1c38b663f21
PressureKun <pressurekun@gmail.com>
2019-12-03 20:07:57 +0300

    added gulp + plugins: browsersync and cssmin

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+
+// Static server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', function () {
+  gulp.src('src/**/*.css')
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'));
+});
