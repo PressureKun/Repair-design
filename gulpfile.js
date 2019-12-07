d06073135ad50b34e972aa777a350f5b0a1a0dc7
PressureKun <pressurekun@gmail.com>
2019-12-07 21:48:44 +0300

    feat: add /grid, normalize, interface, variables/.sass + templates folder with header

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 
 // Static server
- function bs() {
+function bs() {
   serveSass();
   browserSync.init({
     server: {
@@ -17,24 +15,11 @@ const sass = require('gulp-sass');
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
-
-
-
 function serveSass() {
-  return src("src/sass/*.sass")
+  return src("./sass/*.sass")
     .pipe(sass())
-    .pipe(dest("src/css"))
+    .pipe(dest("./css"))
     .pipe(browserSync.stream());
 };
 
-function minify() {
-  src('src/**/*.css')
-    .pipe(cssmin())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(dest('dist'));
-};
-
-exports.min = minify;
 exports.serve = bs;
