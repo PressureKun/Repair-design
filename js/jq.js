f9a8046d81d1381e38c7b1b90bfdce46673f97b7
PressureKun <pressurekun@gmail.com>
2019-12-16 22:32:06 +0300

    feat: js code reworked to jquery + added pageup button

---
+++ js/jq.js
@@ -0,0 +1,24 @@
+// jQuery(document).ready(function () {
+
+//   $(function () {
+//     var element = $("#button-up"),
+//       display;
+//     $(window).scroll(function () {
+//       display = $(this).scrollTop() >= 250;
+//       display != element.css('opacity') && element.stop().animate({
+//         'opacity': display
+//       }, 300);
+//     });
+//   });
+
+//   var btn = $('#button-up');
+
+//   btn.on('click', function (e) {
+//     e.preventDefault();
+//     $('html, body').animate({
+//       scrollTop: 0
+//     }, '300');
+//   });
+
+
+// });
--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+$(document).ready(function () {
+// переменные
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+      btn = $('#button-up');
+// функция модального окна
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible'); 
+// функция кнопки "вверх"
+  });
+  $(function () {
+    var element = $("#button-up"),
+      display;
+    $(window).scroll(function () {
+      display = $(this).scrollTop() >= 250;
+      display != element.css('opacity') && element.stop().animate({
+        'opacity': display
+      }, 300);
+    });
   });
 
-  closeBtn.addEventListener('click', switchModal);
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
 
 });
